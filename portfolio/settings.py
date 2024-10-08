85f42733538bcbbbb4d32af55c8f22c67c0daa6d
Mohamed zoubir <simozoubir012@gmail.com>
2024-10-08 18:30:25 +0100

    adding new updates

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -125,3 +125,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Email settings
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'mohamed.zoubir.dev@gmail.com'  # Your email address
+EMAIL_HOST_PASSWORD = 'your_email_password'  # Your email password
